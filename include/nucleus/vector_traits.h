1a3dcf55bf7bc89b046b1ac5f293e543859d8633
Wenchao Huang <physhuangwenchao@gmail.com>
2026-05-06 23:58:17 +0800

    Add `vector_traits.h`: vec2/3/4_like concepts and scalar_type_t (#10)
    Signed-off-by: Wenchao Huang <physhuangwenchao@gmail.com>

---
+++ include/nucleus/vector_traits.h
@@ -0,0 +1,115 @@
+/**
+ *	Copyright (c) 2025 Wenchao Huang <physhuangwenchao@gmail.com>
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
+ */
+#pragma once
+
+#include "fwd.h"
+#include "vector_types.h"
+#include <type_traits>
+
+#if NS_HAS_CXX_20
+	#include <utility>
+	#include <concepts>
+#endif
+
+namespace NS_NAMESPACE
+{
+	/*****************************************************************************
+	****************************    Vector Concepts    ***************************
+	*****************************************************************************/
+
+#if NS_HAS_CXX_20
+
+	/**
+	 *	@brief	Concept that matches types with exactly two same-typed `x` and `y` components.
+	 *	@note	Satisfied by types such as `float2`, `int2`, `double2`, etc.
+	 *			sizeof(Type) must equal 2 * sizeof(scalar component) to exclude structs with extra fields.
+	 */
+	template<typename Type> concept vec2_like = requires(Type v)
+	{
+		v.x;
+		v.y;
+		requires std::is_same_v<decltype(v.x), decltype(v.y)>;
+		requires sizeof(Type) == 2 * sizeof(decltype(v.x));
+	};
+
+
+	/**
+	 *	@brief	Concept that matches types with exactly three same-typed components `x`, `y` and `z`.
+	 *	@note	Satisfied by types such as `float3`, `int3`, `double3`, etc. Types with a
+	 *			w member (e.g. `float4`) are explicitly excluded, and sizeof(Type) must equal
+	 *			3 * or 4 * sizeof(scalar component) to also cover 16-byte-aligned variants
+	 *			such as `float3_16a` while still excluding structs with too many fields.
+	 */
+	template<typename Type> concept vec3_like = requires(Type v)
+	{
+		v.x;
+		v.y;
+		v.z;
+		requires std::is_same_v<decltype(v.x), decltype(v.y)>;
+		requires std::is_same_v<decltype(v.x), decltype(v.z)>;
+		requires sizeof(Type) == 3 * sizeof(decltype(v.x)) || sizeof(Type) == 4 * sizeof(decltype(v.x));
+	} && !requires(Type v) { v.w; };
+
+
+	/**
+	 *	@brief	Concept that matches types with exactly four same-typed components `x`, `y`, `z` and `w`.
+	 *	@note	Satisfied by types such as `float4`, `int4`, `double4`, etc. sizeof(Type) must equal
+	 *			4 * sizeof(scalar component) to exclude structs with extra fields.
+	 */
+	template<typename Type> concept vec4_like = requires(Type v)
+	{
+		v.x;
+		v.y;
+		v.z;
+		v.w;
+		requires std::is_same_v<decltype(v.x), decltype(v.y)>;
+		requires std::is_same_v<decltype(v.x), decltype(v.z)>;
+		requires std::is_same_v<decltype(v.x), decltype(v.w)>;
+		requires sizeof(Type) == 4 * sizeof(decltype(v.x));
+	};
+
+#endif	//	NS_HAS_CXX_20
+
+	/*****************************************************************************
+	******************************    scalar_type    *****************************
+	*****************************************************************************/
+
+#if NS_HAS_CXX_20
+
+	namespace details
+	{
+		template<typename Type>	 struct VecScalarType;
+		template<vec2_like Type> struct VecScalarType<Type> { using type = std::remove_cvref_t<decltype(std::declval<Type>().x)>; };
+		template<vec3_like Type> struct VecScalarType<Type> { using type = std::remove_cvref_t<decltype(std::declval<Type>().x)>; };
+		template<vec4_like Type> struct VecScalarType<Type> { using type = std::remove_cvref_t<decltype(std::declval<Type>().x)>; };
+	}
+
+	/**
+	 *	@brief	Extracts the scalar type from a vector type.
+	 *	@note	For vector types with an `x` member (e.g. `float2`, `int3`, `double4`),
+	 *			scalar_type_t<T> is the type of that member (e.g. `float`, `int`, `double`).
+	 *			CV-qualifiers and references on T are stripped before deduction.
+	 */
+	template<typename T> using scalar_type_t = typename details::VecScalarType<std::remove_cvref_t<T>>::type;
+
+#endif	//	NS_HAS_CXX_20
+}
---
+++ tests/vector_traits_test.cpp
@@ -0,0 +1,131 @@
+/**
+ *	Copyright (c) 2025 Wenchao Huang <physhuangwenchao@gmail.com>
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
+ */
+
+#include <nucleus/vector_traits.h>
+
+/*********************************************************************************
+***************************    vector_traits_test    *****************************
+*********************************************************************************/
+
+#if NS_HAS_CXX_20
+
+//	--- scalar_type_t ---
+static_assert(std::is_same_v<ns::scalar_type_t<ns::int2>, int>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::int3>, int>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::int4>, int>);
+
+static_assert(std::is_same_v<ns::scalar_type_t<ns::char2>, char>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::char3>, char>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::char4>, char>);
+
+static_assert(std::is_same_v<ns::scalar_type_t<ns::short2>, short>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::short3>, short>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::short4>, short>);
+
+static_assert(std::is_same_v<ns::scalar_type_t<ns::float2>, float>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::float3>, float>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::float4>, float>);
+
+static_assert(std::is_same_v<ns::scalar_type_t<ns::double2>, double>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::double3>, double>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::double4>, double>);
+
+static_assert(std::is_same_v<ns::scalar_type_t<ns::uint2>, unsigned int>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::uint3>, unsigned int>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::uint4>, unsigned int>);
+
+static_assert(std::is_same_v<ns::scalar_type_t<ns::uchar2>, unsigned char>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::uchar3>, unsigned char>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::uchar4>, unsigned char>);
+
+static_assert(std::is_same_v<ns::scalar_type_t<ns::ushort2>,unsigned short>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::ushort3>,unsigned short>);
+static_assert(std::is_same_v<ns::scalar_type_t<ns::ushort4>,unsigned short>);
+
+//	--- scalar_type_t: cv-qualifiers and references are stripped ---
+static_assert(std::is_same_v<ns::scalar_type_t<const ns::float2>, float>);
+static_assert(std::is_same_v<ns::scalar_type_t<const ns::float2&>, float>);
+
+//	--- vec2_like: positive cases ---
+static_assert(ns::vec2_like<ns::int2>);
+static_assert(ns::vec2_like<ns::uint2>);
+static_assert(ns::vec2_like<ns::char2>);
+static_assert(ns::vec2_like<ns::uchar2>);
+static_assert(ns::vec2_like<ns::float2>);
+static_assert(ns::vec2_like<ns::short2>);
+static_assert(ns::vec2_like<ns::ushort2>);
+static_assert(ns::vec2_like<ns::double2>);
+
+//	--- vec2_like: negative cases ---
+static_assert(!ns::vec2_like<ns::float3>);
+static_assert(!ns::vec2_like<ns::float4>);
+static_assert(!ns::vec2_like<float>);
+static_assert(!ns::vec2_like<int>);
+
+//	--- vec2_like: struct with extra fields must not match ---
+struct ExtraField2 { float x, y, extra; };
+static_assert(!ns::vec2_like<ExtraField2>);
+
+//	--- vec3_like: positive cases ---
+static_assert(ns::vec3_like<ns::int3>);
+static_assert(ns::vec3_like<ns::uint3>);
+static_assert(ns::vec3_like<ns::char3>);
+static_assert(ns::vec3_like<ns::float3>);
+static_assert(ns::vec3_like<ns::uchar3>);
+static_assert(ns::vec3_like<ns::short3>);
+static_assert(ns::vec3_like<ns::ushort3>);
+static_assert(ns::vec3_like<ns::double3>);
+static_assert(ns::vec3_like<ns::int3_16a>);
+static_assert(ns::vec3_like<ns::uint3_16a>);
+static_assert(ns::vec3_like<ns::float3_16a>);
+
+//	--- vec3_like: negative cases ---
+static_assert(!ns::vec3_like<ns::float2>);
+static_assert(!ns::vec3_like<ns::float4>);
+static_assert(!ns::vec3_like<float>);
+static_assert(!ns::vec3_like<int>);
+
+//	--- vec3_like: struct with extra fields must not match ---
+struct ExtraField3 { float x, y, z, a, b; };
+static_assert(!ns::vec3_like<ExtraField3>);
+
+//	--- vec4_like: positive cases ---
+static_assert(ns::vec4_like<ns::int4>);
+static_assert(ns::vec4_like<ns::uint4>);
+static_assert(ns::vec4_like<ns::char4>);
+static_assert(ns::vec4_like<ns::uchar4>);
+static_assert(ns::vec4_like<ns::short4>);
+static_assert(ns::vec4_like<ns::float4>);
+static_assert(ns::vec4_like<ns::ushort4>);
+static_assert(ns::vec4_like<ns::double4>);
+
+//	--- vec4_like: negative cases ---
+static_assert(!ns::vec4_like<ns::float2>);
+static_assert(!ns::vec4_like<ns::float3>);
+static_assert(!ns::vec4_like<float>);
+static_assert(!ns::vec4_like<int>);
+
+//	--- vec4_like: struct with extra fields must not match ---
+struct ExtraField4 { float x, y, z, w, extra; };
+static_assert(!ns::vec4_like<ExtraField4>);
+
+#endif	//	NS_HAS_CXX_20
